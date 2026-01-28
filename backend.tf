305677f4c2ee92b89b127289f23baa96752c2482
riyachauhan589 <riyachauhan7521@gmail.com>
2026-01-28 13:40:45 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 terraform {
   backend "s3" {
-    bucket         = "reya-tfstate-bucket-123"
+    bucket         = "reya-tfstate-bucket-1234"
     key            = "3tier/prod/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "dynamodb-terraform-state-lock"
